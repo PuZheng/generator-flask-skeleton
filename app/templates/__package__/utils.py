431259eb1b31c3944a43ecfb559bf5c50b52fb88
xiechao06 <xiechao06@gmail.com>
2015-04-02 18:36:31 +0800

    0

--- app/templates/__package__/utils.py
+++ app/templates/__package__/utils.py
@@ -2,7 +2,8 @@
 import types
 
 
-def do_commit(session, obj, action="add"):
+def do_commit(db, obj, action="add"):
+    session = db.session
     if action == "add":
         if isinstance(obj, types.ListType) or \
            isinstance(obj, types.TupleType):
